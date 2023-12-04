42aad4121c16e43bbd7c1473fa2d0c062b0dd85f
IgorOdaryuk <odaryukigor@gmail.com>
2023-12-04 17:04:27 +0200

    add some files

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({ config })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+  </ChakraProvider>
+  </React.StrictMode>
+);
